9b6d351175cd5cdfb6db48f96037e99bf4e8a95e
Maykel <jotasenator@gmail.com>
2023-10-20 17:01:05 -0400

    Implement hero section Ball Computer on canvas

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { navLinks } from "../constants";
 import { logo, menu, close, jotasLogo } from "../assets";
-import { styles } from "../../styles";
+import { styles } from "../styles";
 
 const Navbar = () => {
   const [ active, setActive ] = useState( "" );
