7cd8c5cb8baa5629019032655f3a2349d687058e
Maykel <jotasenator@gmail.com>
2023-10-20 20:39:00 -0400

    Implement Contact section with earth and stars

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 
 import { BrowserRouter } from 'react-router-dom';
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
+import { About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
 
-function App ()
-{
+function App () {
 
   return (
     <BrowserRouter>
@@ -16,7 +15,6 @@ function App ()
         <Experience />
         <Tech />
         <Works />
-        <Feedbacks />
         <div className='relative z-0'>
           <Contact />
           <StarsCanvas />
--- src/assets/index.js
+++ src/assets/index.js
@@ -21,7 +21,6 @@ import reactjs from "./tech/reactjs.png";
 import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
-import threejs from "./tech/threejs.svg";
 
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
@@ -53,7 +52,6 @@ export {
   redux,
   tailwind,
   typescript,
-  threejs,
   meta,
   shopify,
   starbucks,
